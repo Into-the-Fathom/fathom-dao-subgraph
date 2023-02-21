cb881f08fcfcb87bdd75da47bf6f590e06fff314
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-02-21 14:48:16 +0400

    added separate commands to prepare the diff environments

--- config/apothemAddressScript.ts
+++ config/apothemAddressScript.ts
@@ -6,21 +6,20 @@ import { Addresses } from './addresses.template'
 // mustache doesn't like numbered object keys
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let renameAddresses: any = networkAddresses
-renameAddresses['apothem'] = networkAddresses['51']
+renameAddresses['apothem'] = networkAddresses['51'][process.argv[2]]["addresses"]
+
 
 export let addresses: Addresses = {
   Governor: '{{apothem.Governor}}',
   Staking: '{{apothem.Staking}}',
   VFTHM: '{{apothem.VFTHM}}',
-  blockNumber: '',
-  network: '',
+  blockNumber: networkAddresses['51'][process.argv[2]]["blockNumber"],
+  network: networkAddresses['51']["network"],
 }
 
 const main = (): void => {
   try {
     let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
-    output.blockNumber = '45371000' // Block when first contract was created
-    output.network = 'apothem'
     fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
   } catch (e) {
     console.log(`Error saving artifacts: ${e.message}`)
