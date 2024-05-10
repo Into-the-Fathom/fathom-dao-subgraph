10bc1e3a86b29d96aef321b939174a7150d7b3fb
tarunsharma <tarun.kr.sharma@hotmail.com>
2024-05-10 17:00:37 +0400

    fix: fix graph and ifps node url

---
+++ config/sepoliaAddressScript.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs'
+import * as mustache from 'mustache'
+import * as networkAddresses from '../networks/addresses.json'
+import { Addresses } from './addresses.template'
+
+const SepoliaChainID = '11155111'
+const SepoliaEnvironment = process.argv[2]
+
+// mustache doesn't like numbered object keys
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let renameAddresses: any = networkAddresses
+renameAddresses['sepolia'] = networkAddresses[SepoliaChainID][SepoliaEnvironment]["addresses"]
+
+
+export let addresses: Addresses = {
+  Governor: '{{sepolia.Governor}}',
+  Staking: '{{sepolia.Staking}}',
+  VFTHM: '{{sepolia.VFTHM}}',
+  blockNumber: networkAddresses[SepoliaChainID][SepoliaEnvironment]["blockNumber"],
+  network: networkAddresses[SepoliaChainID]["network"],
+}
+
+const main = (): void => {
+  try {
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+  } catch (e) {
+    console.log(`Error saving artifacts: ${e.message}`)
+  }
+}
+
+main()
